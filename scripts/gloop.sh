254a4cfe7f17c5553182104d92ea927ba45525c0
Kerowak <boiwaq@gmail.com>
2022-01-31 11:08:16 -0500

    Wrote 'gloop', a combo of tar and glob.
    Designed to move our data to Turbo fast & effectively

---
+++ scripts/gloop.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# What do you get when you combine tar and glob? Gloop!
+#
+# This script was originally designed to migrate our data from local machines to
+# Turbo.
+#
+# Given a glob, it will tar the inputs and output them to a provided destination.
+#
+#
+
+PARAMS=""
+
+while (( "$#" )); do
+  case "$1" in
+    -o|--out)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        OUT=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done # set positional arguments in their proper place
+
+eval set -- "$PARAMS"
+
+for path in $PARAMS
+do
+  dirname=$(dirname $path)
+  basename=$(basename $path)
+  output="$OUT$(basename $path).tar.gz"
+  while : ; do
+    if [-f $output]; then
+      # Turbo has this fun feature where files become immutable after they're
+      # written to. So we sometimes run into issues where a handle is dropped and
+      # reacquired in the middle of gzipping an archive. And then we can't write
+      # any more.
+      #
+      # Hence the rm.
+      rm -f $output
+    fi
+
+    tar -I pigz -cvf $output -C $dirname $basename
+
+    if [ $? -eq 0 ]; then
+      break
+    fi
+  done
+done
