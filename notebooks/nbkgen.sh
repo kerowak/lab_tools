963a089d005721f69115310a573ba43f81ff3232
kerowak <boiwaq@gmail.com>
2022-05-09 11:26:29 -0400

    Bugfix for nbkgen.sh
    If jupyter had not yet been run, ~/.jupyter would not exist. The script
    would crash, as it made the implicit assumption that ~/.jupyter would be present.

--- notebooks/nbkgen.sh
+++ notebooks/nbkgen.sh
@@ -4,7 +4,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR && poetry run ipython kernel install --user --name=barma
 
-JUPYTER_CONF=$HOME/.jupyter/jupyter_lab_config.py
+JUPYTER_DIR=$HOME/.jupyter
+if [[ ! -d $JUPYTER_DIR ]]; then
+    mkdir $JUPYTER_DIR
+fi;
+
+JUPYTER_CONF=$JUPYTER_DIR/jupyter_lab_config.py
 if [[ ! -f $JUPYTER_CONF ]]; then
     NB_DIR=$SCRIPT_DIR/notebooks
     echo "c.ServerApp.root_dir = '$NB_DIR'" > $JUPYTER_CONF
