895a33c1242e983de4cd0241281bd5ca395d22f9
kerowak <boiwaq@gmail.com>
2022-05-10 14:01:15 -0400

    Improved compress.sh
    Now it doesn't cause data loss

--- scripts/bash/compress.sh
+++ scripts/bash/compress.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PARAMS=""
 
+stop_script=false
 CPUS=1
 
 while (( "$#" )); do
@@ -28,9 +29,29 @@ done # set positional arguments in their proper place
 eval set -- "$PARAMS"
 
 compress() {
-    if [[ $(identify -verbose $1 | grep Compression | awk '{ print $2 }') == 'None' ]]; then
-        echo "compressing $1"
-        convert $1 -compress lzw $1
+    stop_script=false
+    trap "stop_script=true" SIGINT
+    echo "compressing $1"
+
+    convert $1 -compress lzw $1._tmp
+    if (( $? != 0 )); then
+      if [[ -f $1._tmp ]]; then
+          rm $1._tmp
+      fi;
+      echo "encountered an issue while compressing"
+      return 0
+    fi;
+    if [ $stop_script = true ]; then
+        exit 0
+    fi;
+    initial_size=$(stat --printf="%s" $1)
+    compressed_size=$(stat --printf="%s" $1._tmp)
+
+    # Sometimes lzw will make files bigger :^)
+    if (( compressed_size < initial_size )); then
+      mv $1._tmp $1
+    else
+      rm $1._tmp
     fi;
 }
 
@@ -38,7 +59,9 @@ compress() {
 # funtions for some reason
 export -f compress
 
+echo "running at x$CPUS parallelism"
 for path in $PARAMS
 do
-    find $1 -name *.tif | xargs -l -P $CPUS bash -c 'compress $@' _
+    # Skip over white_light subdir because it's always gonna produce bigger images when compressed with LZW
+    find $1 -name *.tif -not -path "*/white_light/*" | xargs -l -P $CPUS bash -c 'compress $@' _
 done
